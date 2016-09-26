3c1c40f256ca66a9837c8e1830ec97593b24865f
Renato Groffe <renato.groffe@yahoo.com.br>
2016-09-26 01:00:36 -0300

    Add project files.

---
+++ ExemploConsultaCredito/IServicoConsultaCredito.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExemploConsultaCredito
+{
+    public interface IServicoConsultaCredito
+    {
+        IList<Pendencia> ConsultarPendenciasPorCPF(string cpf);
+    }
+}
